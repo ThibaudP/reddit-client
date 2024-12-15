7b07e4f7d68e6e1fc746efec88406d3934922fd7
Thibaud Poncin <t.poncin@gmail.com>
2024-12-15 23:59:53 +0100

    fix: header padding

--- src/app/AppLayout.jsx
+++ src/app/AppLayout.jsx
@@ -8,9 +8,9 @@ function AppLayout() {
     <>
       <div
         id="top"
-        className="w-full h-16 shadow-lg flex mb-5 bg-white rounded-b-lg fixed justify-between"
+        className="w-full h-16 shadow-lg flex mb-5 bg-white rounded-b-lg fixed justify-between pr-4 pl-4"
       >
-        <h1 className="header-title content-center ml-4 font-semibold text-xl">
+        <h1 className="header-title content-center font-semibold text-xl">
           <Link to="/">RedditClient</Link>
         </h1>
         <SearchBar />
